e4e0716b66e64a3161fd9615006d184a3faa8a4f
Matei Marica <matei.marica@outlook.com>
2021-11-22 02:12:26 -0400

    Added support for SSL cert

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,19 @@ require('dotenv').config();
 const express = require('express'),
       path = require('path'),
       logger = require('./middleware/logger');
-
+      http = require('http'),
+      https = require('https'),
+      fs = require('fs');
+      
 const app = express();
+
+let credentials;
+if (process.env.ENV === 'prod') {
+    credentials = {
+        key: fs.readFileSync(process.env.SSL_PRIVATE_KEY),
+        cert: fs.readFileSync(process.env.SSL_CERT)
+    }
+}
 
 // Use logger middleware function to print logs during runtime
 app.use(logger);
@@ -13,4 +24,11 @@ app.use('/api/complaints', require('./routes/api/complaints'));
 // Set static folder
 app.use(express.static(path.join(__dirname, '../', 'frontend')));
 
-app.listen(process.env.PORT, () => console.log(`Server started on port ${process.env.PORT}`));
+let server;
+if (process.env.ENV === 'prod') {
+    server = https.createServer(credentials, app);
+} else {
+    server = http.createServer(app);
+}
+
+server.listen(process.env.PORT, () => console.log(`Server started on port ${process.env.PORT}`));
