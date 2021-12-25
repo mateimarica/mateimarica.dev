66f9478a779a154d837a470a81a1544e7c6b2e2c
Matei Marica <matei.marica@outlook.com>
2021-12-24 20:35:47 -0400

    Finished answers endpoint handler

--- backend/subdomains/qr/routes/api/answers.js
+++ backend/subdomains/qr/routes/api/answers.js
@@ -20,6 +20,7 @@ router.post('/', (req, res) => {
 
 	connectionWrapper((connection) => {
 		let sql = `INSERT INTO answers (answer, questionId, author) VALUES (?, ?, ?);`;
+
 		let params = [answer, questionId, req.body.session.username];
 		connection.execute(sql, params, (err, results) => {
 			if (err) {
@@ -32,6 +33,7 @@ router.post('/', (req, res) => {
 	}, res, false, process.env.QR_DB_NAME);
 });
 
+// Get all answers for a question
 router.get('/', (req, res) => {
 	if (!req.body)
 		return res.sendStatus(400);
@@ -46,11 +48,16 @@ router.get('/', (req, res) => {
 
 	connectionWrapper((connection) => {
 		let sql = 
-			`SELECT * FROM answers WHERE questionId=? ` + 
-			`ORDER BY IFNULL((SELECT SUM(vote) FROM votes WHERE post_id = answers.id), 0) DESC,` + `date_created DESC LIMIT 20;`;
+			`SELECT a.*, CONVERT(COALESCE(SUM(v1.vote), 0), SIGNED) AS votes, v2.vote AS currentUserVote ` +
+			`FROM answers AS a ` +
+			`LEFT OUTER JOIN votes AS v1 ON v1.answerId=a.id ` +
+			`LEFT OUTER JOIN votes AS v2 ON v2.answerId=a.id AND v2.voter=? ` +
+			`WHERE a.questionId=? ` +
+			`GROUP BY a.id ` + 
+			`ORDER BY CONVERT(COALESCE(SUM(v1.vote), 0), SIGNED) DESC LIMIT 15;`;
 
-		let params = [questionId];
-		
+		let params = [req.body.session.username, questionId];
+
 		connection.execute(sql, params, (err, results) => {
 			if (err) {
 				console.log(err);
@@ -59,8 +66,66 @@ router.get('/', (req, res) => {
 
 			res.status(200).json(results);
 		});
-
 	}, res, false, process.env.QR_DB_NAME);
+});
+
+router.patch('/', (req, res) => {
+	if (!req.body || !req.body.params)
+		return res.sendStatus(400);
+
+	if (!users.isSessionValid(req.body.session))
+		return res.sendStatus(401);
+
+	let answer = req.body.params.answer,
+	    id = req.body.params.id;
+
+	if (!answer || answer.length < 1 || answer.length > 3500 || !id) 
+		return res.status(400).send('Missing or out-of-bounds argument(s)');
+	
+	users.isAuthor(req.body.session.username, id, users.postType.ANSWER, res, () => {
+		connectionWrapper((connection) => {
+			let sql = `UPDATE answers SET answer=? WHERE id=?;`;
+			let params = [answer, id];
+
+			connection.execute(sql, params, (err, results) => {
+				if (err) {
+					console.log(err);
+					return res.sendStatus(500);
+				}
+
+				res.sendStatus(204);
+			});
+		}, res, false, process.env.QR_DB_NAME);
+	});
+});
+
+router.delete('/', (req, res) => {
+	if (!req.body || !req.body.params)
+		return res.sendStatus(400);
+
+	if (!users.isSessionValid(req.body.session))
+		return res.sendStatus(401);
+
+	let id = req.body.params.id;
+
+	if (!id)
+		return res.status(400).send('Missing argument');
+	
+	users.isAuthor(req.body.session.username, id, users.postType.ANSWER, res, () => {
+		connectionWrapper((connection) => {
+			let sql = `DELETE FROM answers WHERE id=?;`;
+			let params = [id];
+
+			connection.execute(sql, params, (err, results) => {
+				if (err) {
+					console.log(err);
+					return res.sendStatus(500);
+				}
+
+				res.sendStatus(204);
+			});
+		}, res, false, process.env.QR_DB_NAME);
+	});
 });
 
 module.exports = router;
