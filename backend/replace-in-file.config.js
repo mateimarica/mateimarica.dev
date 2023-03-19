844c5e06cfa3d58372d02055075eb60afcf2687d
Matei Marica <matei.marica@outlook.com>
2023-03-18 22:56:34 -0300

    Removed unused set-local-urls script and all associated files & packages

--- backend/replace-in-file.config.js
+++
@@ -1,6 +0,0 @@
-// Replace all mateimarica.dev links in html files with a local link, for local development
-module.exports = {
-	files: 'frontend_build/**/*.html',
-	from: /mateimarica.dev/g,
-	to: 'mateimarica.local:5000',
-};
